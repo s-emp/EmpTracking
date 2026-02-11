60e4667e47e3675da00b4b3213f64c212d6a6de4
Emp <mpt.s.a.melnikov@gmail.com>
2026-02-11 19:04:54 +0300

    init

---
+++ EmpTracking/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EmpTracking
+//
+//  Created by Emp on 2/11/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ EmpTrackingTests/EmpTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmpTrackingTests.swift
+//  EmpTrackingTests
+//
+//  Created by Emp on 2/11/26.
+//
+
+import Testing
+@testable import EmpTracking
+
+struct EmpTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
